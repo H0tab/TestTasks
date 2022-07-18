c1f531fd5a1208f79b7e84cefbedf7577cfcc648
H0tab <ksasha18@mail.ru>
2022-07-18 23:07:19 +0300

    Swap

--- 3/3/Program.cs
+++ 3/3/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int x = 1;
+int y = 2;
+int z = 3;
+
+Swap(ref x, ref y, ref z);
+
+Console.WriteLine($"x - {x}, y - {y}, z - {z}"); //x - 2, y - 3, z - 1
+
+void Swap(ref int x, ref int y, ref int z)
+{
+    (x, y, z) = (y, z, x);
+}
