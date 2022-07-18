ab74f925263c1e04f30e7183997d1e1526300a31
H0tab <ksasha18@mail.ru>
2022-07-19 01:18:29 +0300

    Tree

--- 1/1/Program.cs
+++ 1/1/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Tree
+{
+    public Tree(string name, List<Tree> children)
+    {
+        Name = name;
+        Children = children;
+    }
+
+    public string Name { get; set; }
+    public IEnumerable<Tree> Children { get; set; }
+
+    public void GetAllNames(Tree tree, List<string> result)
+    {
+        result.Add(tree.Name);
+
+        if (tree.Children == null)
+        {
+            return;
+        }
+
+        foreach (var child in tree.Children)
+        {
+            GetAllNames(child, result);
+        }
+    }
+}
